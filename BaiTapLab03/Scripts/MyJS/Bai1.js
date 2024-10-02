c4a8b6a77ed141ae7e144f47dff925a1b487bcc8
vanh <vanh@VANHHEEE>
2024-10-02 22:10:23 +0700

    Add project files.

---
+++ BaiTapLab03/Scripts/MyJS/Bai1.js
@@ -0,0 +1,40 @@
+﻿function getValue(id) {
+    return document.getElementById(id).value.trim();
+}
+function showError(key, mess) {
+    document.getElementById(key + '_error').innerHTML = mess;
+}
+
+function validate() {
+    var flag = true;
+    var studentId = getValue('studentId');
+    if (studentId.length != 10 || !/^[a-zA-Z0-9]+$/.test(studentId)) {
+        flag = false;
+        showError('studentId', 'Mã sinh viên gồm 10 ký tự')
+    }
+    var name = getValue("name");
+    if (name == '' || name.length < 30 || !/^[a-zA-Z0-9]+$/.test(name)) {
+        flag = false;
+        showError('name', 'Họ tên không rỗng và < 30 ký tự');
+    }
+    var age = getValue('age');
+    if (age > 18) {
+        flag = false;
+        showError('age', 'Tuổi phải 18 trở lên');
+    }
+    var language = getValue('language');
+    for (var i = 0; i < language.length; i++) {
+        if (language[i].checked == true) {
+            showError('language', language[i].value);
+        }
+    }
+}
+function majorOption() {
+    var major = document.getElemntById('major').value;
+    var specialty = '';
+    switch (major) {
+        case 'Hệ thống': specialty = 'Phân tích và thiết kế';
+        case 'Phần mềm': specialty = 'Lập trình';
+        case 'Mạng máy tính': specialty = 'Quản lý mạng';
+    }
+}
---
+++ BaiTapLab03/Scripts/MyJS/Bai2.js
@@ -0,0 +1,56 @@
+﻿function getValue(id) {
+    return document.getElementById(id).value.trim();
+}
+
+function showError(key, mess) {
+    document.getElementById(key + '_error').innerHTML = mess;
+}
+
+function validate() {
+    var flag = true;
+    var ten = getValue("iName");
+    if (ten == ' ') {
+        flag = false;
+        showError(ten, 'Vui lòng kiểm tra lại Name');
+    }
+    var diachi = getValue("iAddress");
+    if (diachi == ' ') {
+        flag = false;
+        showError(diachi, 'Vui lòng kiểm tra lại Name');
+    }
+    var sdt = getValue("iTel");
+    if (sdt == ' ') {
+        flag = false;
+        showError(sdt, 'Vui lòng kiểm tra lại Name');
+    }
+    var thanhpho = getValue("iCity");
+    if (thanhpho == ' ') {
+        flag = false;
+        showError(thanhpho, 'Vui lòng kiểm tra lại Name');
+    }
+    var quan = getValue("iDistrict");
+    if (quan == ' ') {
+        flag = false;
+        showError(quan, 'Vui lòng kiểm tra lại Name');
+    }
+    var phuong = getValue("iCommune");
+    if (phuong == ' ') {
+        flag = false;
+        showError(phuong, 'Vui lòng kiểm tra lại Name');
+    }
+    return flag;
+}
+function Thongbao1() {
+    alert("Bạn chọn giao hàng tại văn phòng");
+}
+function Thongbao2() {
+    alert("Bạn chọn giao hàng tại nhà riêng");
+}
+function Luuthongtin() {
+    if (validate() == false) {
+        alert("Thông tin nhập không hợp lệ");
+    }
+    else {
+        alert("Lưu thông tin thành công");
+    }
+}
